dc61ab830eb1a1d5a9e7428921caa14d4f22c536
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-06 09:22:01 +0100

    update domain-model version; add ARMv8 compilation

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+PV=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64
 docker login quay.io -u ${QUAY_MANUFACTURING_USERNAME} -p ${QUAY_MANUFACTURING_PASSWORD}
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker build -t quay.io/qiotmanufacturing/factory-production-validator:$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64 -f src/main/docker/Dockerfile.native.multiarch .
-docker push quay.io/qiotmanufacturing/factory-production-validator:$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64
+docker rmi quay.io/qiotmanufacturing/factory-production-validator:$PV --force
+docker build -t quay.io/qiotmanufacturing/factory-production-validator:$PV -f src/main/docker/Dockerfile.native.multiarch .
+docker push quay.io/qiotmanufacturing/factory-production-validator:$PV
--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
+
 mvn -B clean package -Pprod,native oc:build oc:push \
           -Dquarkus.native.container-build=true \
           -Dquarkus.container-image.build=true \
           -Djkube.docker.push.username=${QUAY_MANUFACTURING_USERNAME} \
           -Djkube.docker.push.password=${QUAY_MANUFACTURING_PASSWORD} 
-
-# docker run --rm --privileged multiarch/qemu-user-static:register --reset
-# docker build -t quay.io/qiotmanufacturing/factory-production-validator:1.0.0-beta1-aarch64 -f src/main/docker/Dockerfile.native.multiarch .
-# docker push quay.io/qiotmanufacturing/factory-production-validator:1.0.0-beta1-aarch64
